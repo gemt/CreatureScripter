a468963dd78efc47c112aa9193e233416c2697f9
skarseth <skarsethkristian@gmail.com>
2017-04-30 19:00:26 +0200

    widgetfactory cleanup

--- src/ScriptAI/widgets/widgetFactory.h
+++ src/ScriptAI/widgets/widgetFactory.h
@@ -9,15 +9,22 @@
 #include "spellidwidget.h"
 #include "defaultlineedit.h"
 #include "typevaluewidget.h"
+#include "eventwidgetclasses.h"
 
 static QWidget* GetParameterWidget(const EventAI::Parameter& param, QSqlRecord& record, const QString& field, QWidget* parent = nullptr){
     switch(param.type){
+    case EventAI::MILLISECONDS: return new EventAI::type_MS(r, idx);
+    case EventAI::PERCENTAGE: return new DefaultLineEdit(record, field, param, parent);
+    case EventAI::SPELL_ID: return new SpellIDWidget(record, field, param, parent);
+    case EventAI::FACTION_ID_FLAGS: return new TypeValueWidget(EventAI::factionFlags, record, field, param, parent);
+    case EventAI::TARGET: return new TypeValueWidget(EventAI::TargetTypes, record, field, param, parent);
+    case EventAI::CAST_FLAGS: return new TypeValueWidget(EventAI::CastFlags, record, field, param, parent);
+    case EventAI::SHEET: return new TypeValueWidget(EventAI::SheetState, record, field, param, parent);
+    case EventAI::EVENT_TYPE_MASK: return new TypeValueWidget(EventAI::EventTypeMask, record, field, param, parent);
+    case EventAI::STAND_STATE: return new TypeValueWidget(EventAI::StandState, record, field, param, parent);
+    case EventAI::MOVEMENT_TYPE:return new TypeValueWidget(EventAI::MovementType, record, field, param, parent);
+    case EventAI::REACT_STATE: return new TypeValueWidget(EventAI::ReactState, record, field, param, parent);
     case EventAI::UNUSED:
-    case EventAI::MILLISECONDS:
-    case EventAI::PERCENTAGE:
-        return new DefaultLineEdit(record, field, param, parent);
-    case EventAI::SPELL_ID:
-        return new SpellIDWidget(record, field, param, parent);
     case EventAI::SPELL_SCHOOL:
     case EventAI::DISTANCE:
     case EventAI::BOOL:
@@ -30,11 +37,8 @@ static QWidget* GetParameterWidget(const EventAI::Parameter& param, QSqlRecord& 
     case EventAI::CONDITION:
     case EventAI::TEXT_ID:
     case EventAI::FACTION_ID:
-    case EventAI::FACTION_ID_FLAGS: return new TypeValueWidget(EventAI::factionFlags, record, field, param, parent);
     case EventAI::MODEL_ID:
     case EventAI::SOUND_ID:
-    case EventAI::TARGET: return new TypeValueWidget(EventAI::TargetTypes, record, field, param, parent);
-    case EventAI::CAST_FLAGS: return new TypeValueWidget(EventAI::CastFlags, record, field, param, parent);
     case EventAI::QUEST_ID:
     case EventAI::FLAGS_GENERIC:
     case EventAI::PHASE:
@@ -43,12 +47,7 @@ static QWidget* GetParameterWidget(const EventAI::Parameter& param, QSqlRecord& 
     case EventAI::TEAM:
     case EventAI::CREATURE_TEMPLATE_ID:
     case EventAI::RADIUS:
-    case EventAI::SHEET: return new TypeValueWidget(EventAI::SheetState, record, field, param, parent);
     case EventAI::CHANCE:
-    case EventAI::EVENT_TYPE_MASK: return new TypeValueWidget(EventAI::EventTypeMask, record, field, param, parent);
-    case EventAI::STAND_STATE: return new TypeValueWidget(EventAI::StandState, record, field, param, parent);
-    case EventAI::MOVEMENT_TYPE:return new TypeValueWidget(EventAI::MovementType, record, field, param, parent);
-    case EventAI::REACT_STATE: return new TypeValueWidget(EventAI::ReactState, record, field, param, parent);
         return new DefaultLineEdit(record, field, param, parent);
     }
 }
